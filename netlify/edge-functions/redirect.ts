8fded52918b1f0dab1c7dca9bf5fa9ddf4db2f8a
luca319 <lucasaeoninsurance@gmail.com>
2025-01-02 21:01:12 +0800

    Update redirect.ts

--- netlify/edge-functions/redirect.ts
+++ netlify/edge-functions/redirect.ts
@@ -8,21 +8,30 @@ export default async function handler(request: Request, context: Context) {
   const html = `<!DOCTYPE html>
 <html prefix="og: https://ogp.me/ns#">
   <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8">
     <title>Rich Recipe</title>
-    <meta property="og:title" content="Rich Recipe" />
+    <meta property="og:title" content="Rich Products Recipe" />
+    <meta property="og:description" content="Rich Products Recipe Document" />
     <meta property="og:type" content="website" />
-    <meta property="og:image" content="${decodeURIComponent(thumb || '')}" />
-    <meta http-equiv="refresh" content="0;url=${decodeURIComponent(pdf || '')}" />
+    <meta property="og:image" content="${thumb ? decodeURIComponent(thumb) : ''}" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image" content="${thumb ? decodeURIComponent(thumb) : ''}" />
+    <script>
+      window.location.href = "${pdf ? decodeURIComponent(pdf) : ''}";
+    </script>
   </head>
   <body>
-    Redirecting...
+    Redirecting to document...
   </body>
 </html>`;
 
   return new Response(html, {
     headers: {
-      'content-type': 'text/html',
+      'content-type': 'text/html;charset=UTF-8',
+      'cache-control': 'no-cache',
+      'x-robots-tag': 'noindex'
     },
   });
 }
